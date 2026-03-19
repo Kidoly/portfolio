fc70b6214915f7f3f9cadf4b056f4cd141ecb356
Kidoly <mistercreepreal@gmail.com>
2026-03-19 01:36:15 +0100

    fix: add plausible to csp

--- next.config.js
+++ next.config.js
@@ -29,11 +29,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.io",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
               "font-src 'self' data:",
-              "connect-src 'self'",
+              "connect-src 'self' https://plausible.io",
               "media-src 'self'",
               "object-src 'none'",
               "frame-src 'none'",
